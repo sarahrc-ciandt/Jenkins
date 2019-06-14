fb1c2a629406636da99594a43b1513208a642048
Sarah Roland Campos Lisboa <sarahrc@lnxcit020068.cit>
2019-06-14 14:48:23 -0300

    fixing some erros

--- dockerRunApplication.sh
+++ dockerRunApplication.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+echo 'Building Postgres Image...'
 docker build -t postgres_db -f db/Dockerfile .
 
-docker run -it --rm --name docker-compose1 \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "$(pwd)":/usr/src/mydocker \
-    -w /usr/src/mydocker \
-    dduportal/docker-compose \
-    up -d
+echo 'Running docker-compose...'
+for value in {1..2}
+do
+    docker run -it --rm --name docker-compose1 \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v "$(pwd)":/usr/src/mydocker \
+        -w /usr/src/mydocker \
+        dduportal/docker-compose \
+        up -d
+done
 
      
--- dockerRunSonar.sh
+++ dockerRunSonar.sh
@@ -2,6 +2,7 @@
 
 docker run -it --rm --name my-maven-project  \
     -v "$(pwd)/SonarApplication":/usr/src/mymaven \
-    -w /usr/src/mymaven maven:3.6-jdk-8-alpine \
+    -w /usr/src/mymaven \
+    maven:3.6-jdk-8-alpine \
     mvn sonar:sonar \
     -Dsonar.host.url=http://172.17.0.3:9000
