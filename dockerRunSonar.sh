5c8c1b087bc74670ec2d084cd47c750e82920a00
Sarah Roland Campos Lisboa <sarahrc@lnxcit020068.cit>
2019-06-19 14:30:38 -0300

    1

--- dockerRunSonar.sh
+++ dockerRunSonar.sh
@@ -3,8 +3,7 @@
 docker run -i --rm --name maven-project  \
     -v /var/lib/docker/volumes/jenkins-data/_data/workspace/JavaApplication/SonarApplication:/usr/src/maven-project \
     -w /usr/src/maven-project \
-    maven:ibmjava-alpine \    
-    mvn sonar:sonar \
-    -Dsonar.host.url=http://172.17.0.3:9000
+    maven:ibmjava-alpine \
+    mvn sonar:sonar -Dsonar.host.url=http://172.17.0.3:9000
 
 
