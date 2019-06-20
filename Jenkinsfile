3c9b5882522b42e8d9e78dec0b655b2dbb7c6883
Sarah Roland Campos Lisboa <sarahrc@lnxcit020068.cit>
2019-06-20 12:09:58 -0300

    1

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,7 +3,10 @@ node() {
         checkout scm   
             
     stage 'Running Sonar'
-        sh './dockerRunSonar.sh'
+
+        docker.image('maven:ibmjava-alpine').withRun('-v /var/lib/docker/volumes/jenkins-data/_data/workspace/JavaApplication/SonarApplication:/usr/src/maven-project -w /usr/src/maven-project') { c ->
+            sh 'mvn sonar:sonar -Dsonar.host.url=http://172.17.0.3:9000'        
+        }
 
     stage 'Java Application'
         sh './dockerRunApplication.sh' 
--- dockerRunApplication.sh
+++ dockerRunApplication.sh
@@ -4,15 +4,11 @@ echo 'Building Postgres Image...'
 docker build -t postgres_db -f db/Dockerfile .
 
 echo 'Running docker-compose...'
-for value in {1..2}
-do
-    docker run -i --rm --name docker-compose1 \
-        -v /var/run/docker.sock:/var/run/docker.sock \
-        -v /var/lib/docker/volumes/jenkins-data/_data/workspace/JavaApplication:/usr/src/mydocker \
-        -w /usr/src/mydocker \
-        dduportal/docker-compose \
-        up -d
-
-done
+docker run -i --rm --name docker-compose1 \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /var/lib/docker/volumes/jenkins-data/_data/workspace/JavaApplication:/usr/src/mydocker \
+    -w /usr/src/mydocker \
+    dduportal/docker-compose \
+    up -d
 
      
