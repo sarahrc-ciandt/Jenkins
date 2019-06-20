dea24bc4180a19f9b9d5af204f39ea3318401265
Sarah Roland Campos Lisboa <sarahrc@lnxcit020068.cit>
2019-06-20 15:36:37 -0300

    1

--- migration.sh
+++ migration.sh
@@ -4,9 +4,9 @@ echo "Coping Files..."
 docker cp db/acesso_init.sql data_base:opt/acesso_init.sql
 
 echo 'Executing Migration ..'
-(docker exec data_base su - postgres && \
+docker exec data_base bash -c 'su - postgres && \
     PGPASSWORD=postgres psql \
     --host=localhost \
     --port=5432 \
     --username=postgres \
-    --file db/acesso_init.sql)
+    --file /opt/acesso_init.sql'
